41f4b8c058bec3f7e694a0c6ba6a0c145da1bbca
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-03-31 21:52:20 -0300

    Changed timestamp set method

--- update.lua
+++ update.lua
@@ -29,7 +29,7 @@
 --
 
 filetimestamp = function(file)
-    return io.popen('stat -c %w "' .. file .. '"'):read("*a")
+    return io.popen('grep "' .. file .. '" timestamps.txt | cut -d" " -f1-3'):read("*a")
 end
 
 l = io.popen("ls -c _markdown")
